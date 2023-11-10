641a14ab77940ef41baa1b03916ba968ace2f3e3
ghosts-cold <ghosts-cold1991@ya.ru>
2023-11-10 14:20:30 +0700

    Initial Commit

---
+++ RectangleAniation/RectangleAniationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleAniationApp.swift
+//  RectangleAniation
+//
+//  Created by Дима Васильев on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RectangleAniationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
